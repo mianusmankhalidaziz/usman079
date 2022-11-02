2f34d20dd86e4cb97438b70175d342143243b49f
USMAN KHALID <ua8046434@gmail.com>
2022-11-02 19:56:36 +0500

    Error 2nd video
    There is error on code
    
    Co-Authored-By: githubprojectmine <79458502+githubprojectmine@users.noreply.github.com>

--- bmi-calculator-flutter-master/lib/main.dart
+++ bmi-calculator-flutter-master/lib/main.dart
@@ -27,7 +27,7 @@ class _InputPageState extends State<InputPage> {
         child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+       
       ),
     );
   }
