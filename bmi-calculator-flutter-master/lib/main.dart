b060d053601d74e5d1a6284216a8d1b55bdb7876
USMAN KHALID <ua8046434@gmail.com>
2022-11-10 19:22:50 +0500

    Error in Video lecture 57 part 4
    mainAxisAlignment.center ka issue ka arha hy sir error aarha hy

--- bmi-calculator-flutter-master/lib/main.dart
+++ bmi-calculator-flutter-master/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -29,54 +29,59 @@ class _InputPageState extends State<InputPage> {
             children: [
               Expanded(child:
 
-                Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius:BorderRadius.circular(10.0),
-
-                  ),
+                RepeatContainerCode(
+                  colors : Color(0xFF1D1E33),
                 ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius:BorderRadius.circular(10.0),
+              Expanded(child:RepeatContainerCode(
+                colors : Color(0xFF1D1E33),
+                  cardWigdet: Column(
+                    mainAxisAlignment: .center,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.male,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('MALE',
+                        style: TextStyle(
+                        fontSize: 18.0,
+                      ),
 
-                ),
+                      ),
+                    ],
+                  ),
               ),),
             ],
           ),),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius:BorderRadius.circular(10.0),
-
-            ),
-          ),),
+          Expanded(child:RepeatContainerCode(colors : Color(0xFF1D1E33),),),
           Expanded(child:  Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius:BorderRadius.circular(10.0),
-
-                ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius:BorderRadius.circular(10.0),
-
-                ),
-              ),),
+              Expanded(child: RepeatContainerCode(colors : Color(0xFF1D1E33),),),
+              Expanded(child: RepeatContainerCode(colors : Color(0xFF1D1E33),),),
             ],
           ),),
         ],
       )
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({@required this.colors, this.cardWigdet});
+  final Color colors;
+  final Widget cardWigdet;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: cardWigdet,
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius:BorderRadius.circular(10.0),
+
+      ),
     );
   }
 }
